884701c7ece656a4c17153c0e5e08c8255ab1567
minseo_song <phmsoft30@gmail.com>
2024-05-24 11:53:36 +0900

    playerJump & playerFalling anim

--- JumpForChicken/Assets/playerController.cs
+++ JumpForChicken/Assets/playerController.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
     public float movePower = 1f;
-    public float jumpPower = 1f;
+    public float jumpPower = 0.05f;
+    public float minJumpPower = 2f;
+    public float maxJumpPower = 5f;
     Rigidbody2D rigid;
     Animator animator_;
 
@@ -13,7 +16,10 @@ public class playerController : MonoBehaviour
     public int lookat = 1;
 
     Vector3 movement;
+    // 점프 힘 조절
+    float jumpGauge = 0f;
     bool isJump = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,13 +27,18 @@ public class playerController : MonoBehaviour
         animator_ = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    // 프레임 당 초기화 : 사용자 인풋 감지를 위함
     void Update()
     {
-        if (Input.GetButtonDown("Jump") && !animator_.GetBool("isJumping")) {
+        if (Input.GetButton("Jump") && !animator_.GetBool("isJumping")) {
+            jumpGauge += jumpGauge < maxJumpPower ? jumpPower : 0;
+        }
+        if (Input.GetButtonUp("Jump") && !animator_.GetBool("isJumping") && jumpGauge >= minJumpPower){
             isJump = true;
             animator_.SetBool("isJumping", true);
             animator_.SetTrigger("doJumping");
+        } else if (Input.GetButtonUp("Jump") && !animator_.GetBool("isJumping") && jumpGauge < minJumpPower){
+            jumpGauge = 0f;
         }
     }
 
@@ -50,7 +61,7 @@ public class playerController : MonoBehaviour
             lookat = 1;
         }
         transform.localScale = new Vector3(lookat, 1, 1);
-        transform.position += moveVelocity * movePower * Time.deltaTime;
+        transform.position += jumpGauge == 0 ? moveVelocity * movePower * Time.deltaTime : Vector3.zero;
     }
 
     void Jump()
@@ -60,25 +71,34 @@ public class playerController : MonoBehaviour
         }
         rigid.velocity = Vector2.zero;
 
-        Vector2 jumpVelocity = new Vector2(0, jumpPower);
+        Vector2 jumpVelocity = new Vector2(0, jumpGauge);
         rigid.AddForce(jumpVelocity, ForceMode2D.Impulse);
 
+        jumpGauge = 0f;
         isJump = false;
     }
 
     void Animations()
     {
-        if (Input.GetAxisRaw("Horizontal") == 0){
+        if (Input.GetAxisRaw("Horizontal") == 0 || jumpGauge != 0){
             animator_.SetBool("isMove", false);
         }
-        else if (Input.GetAxisRaw("Horizontal") != 0) {
+        else if (Input.GetAxisRaw("Horizontal") != 0 && jumpGauge == 0) {
             animator_.SetBool("isMove", true);
+        }
+
+        if(rigid.velocity.y < 0){
+            if (!animator_.GetBool("isFalling")){
+                animator_.SetTrigger("doFalling");
+            }
+            animator_.SetBool("isJumping", false);
+            animator_.SetBool("isFalling", true);
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.layer == 6 && rigid.velocity.y < 0) {
-            animator_.SetBool("isJumping", false);
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.gameObject.layer == 6 && rigid.velocity.y == 0) {
+            animator_.SetBool("isFalling", false);
         }
     }
 }
