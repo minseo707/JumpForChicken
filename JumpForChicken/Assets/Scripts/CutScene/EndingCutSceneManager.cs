1ad6c764fd9c0e7a52828e7ea7fe16ffb32e4fee
minseo_song <phmsoft30@gmail.com>
2025-04-11 20:54:51 +0900

    feat: 엔딩 씬 Skip 기능 추가

--- JumpForChicken/Assets/Scripts/CutScene/EndingCutSceneManager.cs
+++ JumpForChicken/Assets/Scripts/CutScene/EndingCutSceneManager.cs
@@ -27,17 +27,34 @@ public class EndingCutSceneManager : MonoBehaviour
 
     private int activePage = 0;
 
+    private int part;
+    private bool skip;
+
 
     /// <summary>
     /// 컷씬 스킵 (스크립트 내에서 재사용 가능)
     /// </summary>
     public void SkipCutScene(){
+        if (part == 1){
+            skip = true;
+        }
 
+        if (part == 2){
+            // 엔딩 컷씬 종료
+            darkPanel.SetActive(false); // 미리 검은 색 불투명 배경 제거
+
+            // TODO: Gameover UI 활성화
+            gameoverUI.SetActive(true);
+
+            fadeTimer = fadeoutDuration;
+            pageTimer = 0f; // Goto (3)
+        }
     }
 
     void Start()
     {
         images = new Image[pages.Length];
+        skip = false;
 
         for (int i = 0; i < pages.Length; i++)
         {
@@ -47,7 +64,7 @@ public class EndingCutSceneManager : MonoBehaviour
 
     void OnEnable()
     {
-        Debug.Log("[EndingCutSceneManager] Point 0");
+        part = 0;
         waitTimer = waitDuration;
         // GameManager에서 데이터 가져오기 (DataManager에서 데이터 저장도 함께 구현)
         activePage = GameManager.GetEndingPage();
@@ -56,6 +73,7 @@ public class EndingCutSceneManager : MonoBehaviour
     void Update()
     {
         if (waitTimer > 0f){
+            part = 0;
             waitTimer -= Time.fixedDeltaTime;
             if (waitTimer <= 0f){
                 pages[activePage].SetActive(true);
@@ -66,6 +84,7 @@ public class EndingCutSceneManager : MonoBehaviour
         }
 
         if (pageTimer == 0f && fadeTimer > 0f && waitTimer != -1f){ // (1)
+            part = 1;
             // 페이지 활성화
             fadeTimer -= Time.fixedDeltaTime;
             float fadeAlpha = 1f - fadeTimer / fadeoutDuration; // 0f -> 1f FadeIn
@@ -78,9 +97,10 @@ public class EndingCutSceneManager : MonoBehaviour
         }
 
         if (pageTimer > 0f && fadeTimer == 0f){ // (2)
+            part = 2;
             pageTimer -= Time.fixedDeltaTime;
             waitTimer = -1f; // (3) -> (1)로 가는 것을 방지하는 트리거
-            if (pageTimer <= 0f){
+            if (pageTimer <= 0f || skip){
                 // 엔딩 컷씬 종료
                 darkPanel.SetActive(false); // 미리 검은 색 불투명 배경 제거
 
@@ -93,6 +113,7 @@ public class EndingCutSceneManager : MonoBehaviour
         }
 
         if (pageTimer == 0f && fadeTimer > 0f && waitTimer == -1f){ // (3)
+            part = 3;
             fadeTimer -= Time.fixedDeltaTime;
             float fadeAlpha = fadeTimer / fadeoutDuration; // 1f -> 0f FadeOut
             images[activePage].color = new Color(1, 1, 1, fadeAlpha);
@@ -102,6 +123,7 @@ public class EndingCutSceneManager : MonoBehaviour
         }
 
         if (pageTimer == 0f && fadeTimer == 0f && waitTimer == -1f){ // (4)
+            part = 4;
             // 완전히 종료
             // TODO: Gameover UI 점수 증가 애니메이션 재생
             gameoverUIManager.increase = true;
